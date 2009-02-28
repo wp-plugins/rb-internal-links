56edab208d23d27d76dd353adfbee97053791575
rb-cohen <rb-cohen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-28 18:37:47 +0000

    Tag 2.0.3, tinymce fix
    git-svn-id: https://plugins.svn.wordpress.org/rb-internal-links/tags/2.0.3@98459 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- rb-internal-links.php
+++ rb-internal-links.php
@@ -13,7 +13,7 @@
  Plugin Name: RB Internal Links
  Plugin URI: http://www.blograndom.com/blog/
  Description: Link to other blog posts and pages without specifying the full URL. Uses a UI to ease finding the post or page you want to link to.
- Version: 2.0.2
+ Version: 2.0.3
  Author: Cohen
  Author URI: http://www.blograndom.com
 
@@ -45,7 +45,10 @@
 if(floatval(phpversion()) < 5)
 	die('You must have PHP version 5+ to use RB Internal Links');
 
+// start plugin
 add_action('init', array('Rb_Internal_Links', 'enable'));
+// add tinymce button to wysiwyg editor
+add_action('init', array('Rb_Internal_Links', 'addWysiwygFilters'));
 
 class Rb_Internal_Links{
 	
@@ -66,8 +69,6 @@ class Rb_Internal_Links{
 		add_shortcode('intlink', array(__CLASS__, 'shortcode'));
 		// add link to settings menu for plugin
 		add_action('admin_menu', array(__CLASS__, 'addOptionsPages'));
-		// add tinymce button to wysiwyg editor
-		add_action('admin_init', array(__CLASS__, 'addWysiwygFilters'));
 	}
 	
 	/**
