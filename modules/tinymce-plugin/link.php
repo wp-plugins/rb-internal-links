e39bff58cb33b90f332a60b0fff95e14e3fdb260
rb-cohen <rb-cohen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-07 17:20:17 +0000

    Empty shortcode string {{empty}} given
    git-svn-id: https://plugins.svn.wordpress.org/rb-internal-links/trunk@382202 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/tinymce-plugin/link.php
+++ modules/tinymce-plugin/link.php
@@ -7,35 +7,28 @@ $linkClass = request('linkClass');
 $linkTarget = request('linkTarget');
 $linkAnchor = request('linkAnchor');
 
-if(empty($linkId) || empty($linkType))
-	$return = array('status' => 'error', 'message' => __('Incorrect link id or type, please try again.'));
-else
-{
-	$code = '[intlink';
-	$code .= ' id="' . $linkId . '"';
-	$code .= ' type="' . $linkType . '"';
-	
-	$code .= addCodeAttr('class', $linkClass);
-	$code .= addCodeAttr('target', $linkTarget);
-	$code .= addCodeAttr('anchor', $linkAnchor);
+if (empty($linkId) || empty($linkType))
+    $return = array('status' => 'error', 'message' => __('Incorrect link id or type, please try again.'));
+else {
+    $code = '[intlink';
+    $code .= ' id="' . $linkId . '"';
+    $code .= ' type="' . $linkType . '"';
 
-	$code .= empty($linkText)? ' /' : '';
-	$code .= ']';
-	
-	if(!empty($linkText))
-	{
-		$code .= $linkText;
-		$code .= '[/intlink]';
-	}
-	
-	$return = array('status' => 'success', 'code' => $code);
+    $code .= addCodeAttr('class', $linkClass);
+    $code .= addCodeAttr('target', $linkTarget);
+    $code .= addCodeAttr('anchor', $linkAnchor);
+
+    $code .= ']';
+
+    $code .= empty($linkText) ? '{{empty}}' : $linkText;
+    $code .= '[/intlink]';
+
+    $return = array('status' => 'success', 'code' => $code);
 }
 
 die(json_encode($return));
 
-
-function addCodeAttr($attr, $value)
-{
-	if(!empty($value))
-		return ' ' . $attr . '="' . $value . '"';
+function addCodeAttr($attr, $value) {
+    if (!empty($value))
+        return ' ' . $attr . '="' . $value . '"';
 }
